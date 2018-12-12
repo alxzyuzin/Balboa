2e1f8a3053b394aae9ac25336aa91e835d4caba0
Alexander Zyuzin <alx.zyuzin@gmail.com>
2018-12-12 11:39:57 +0200

    Add docs folder to project

---
+++ Balboa/Common/BalboaExeption.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Balboa.Common
+{
+    public class BalboaException : Exception
+    {
+        public BalboaException()
+        { }
+
+
+        public BalboaException(string description)
+        {
+            Description = description;
+        }
+
+
+        public BalboaException(string description, Exception exception)
+        {
+            Description = description;
+            PreviousException = exception;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fname">
+        /// Имя файла в котором возникло исключение
+        /// </param>
+        /// <param name="cname">
+        /// /// Имя класса в котором возникло исключение
+        /// </param>
+        /// <param name="mname">
+        /// /// Имя метода в котором возникло исключение
+        /// </param>
+        /// <param name="lnumber">
+        /// Номер строки в файле в которой создаётся объект исключения
+        /// </param>
+        /// <param name="message">
+        /// Описание причины исключения
+        /// </param>
+        public BalboaException(string fileName, string className, string methodName, string lineNumber, string description)
+        {
+            FileName = fileName;
+            ClassName = className;
+            MethodName = methodName;
+            LineNumber = lineNumber;
+            Description = description;
+        }
+
+        public BalboaException(string fileName, string methodName, string lineNumber, string description)
+        {
+            FileName = fileName;
+            MethodName = methodName;
+            LineNumber = lineNumber;
+            Description = description;
+        }
+
+        public string FileName {get;set; } = string.Empty;
+        public string ClassName { get; set; } = string.Empty;
+        public string MethodName { get; set; } = string.Empty;
+        public string LineNumber { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Exception PreviousException { get; set; } = null;
+
+
+    }
+
+    public class BalboaNullValueException: BalboaException
+    {
+
+        public string VariableName { get; set; } = string.Empty;
+
+        public BalboaNullValueException():base()
+        { }
+
+        public BalboaNullValueException(string fileName, string methodName, string lineNumber, string variableName)
+            :base(fileName, methodName, lineNumber, string.Empty)
+        {
+            VariableName = variableName;
+        }
+   
+        public BalboaNullValueException(string description): base(description)
+        { }
+
+        public BalboaNullValueException(string description, Exception exception): base (description, exception)
+        { }
+
+    }
+}
---
+++ Balboa/Common/Log.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Balboa.Common
+{
+    public delegate void EventHandler<TLogEventArgs>(object sender, TLogEventArgs eventArgs);
+    public class Log
+    {
+        public event EventHandler LogError;
+
+        private void NotifyLogError(string message)
+        {
+            if (LogError != null)
+            {
+                LogError(this, new LogEventArgs(message));
+            }
+        }
+
+        public enum LogLevel { NoLog, LogErrorsOnly, LogAllEvents }
+
+        string _FileName = "Balboa.log";
+        StorageFolder _Folder = ApplicationData.Current.LocalFolder;
+        CreationCollisionOption _Options = CreationCollisionOption.ReplaceExisting;
+        StorageFile _File = null;
+
+        public bool Initialized { get; private set; } = false;
+
+        public async Task<bool> Init()
+        {
+            try
+            {
+                await _Folder.GetFileAsync(_FileName);
+                Initialized = true;
+                return true;
+            }
+            catch (FileNotFoundException)
+            {
+                try
+                {
+                    _File = await _Folder.CreateFileAsync(_FileName, _Options);
+                    Initialized = true;
+                    return true;
+                }
+                catch(Exception ex)
+                {
+                    Initialized = false;
+                    NotifyLogError("Error creating log file.\n  Exception: " + ex.GetType().Name + "\n" + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        public async Task Clear()
+        {
+            try
+            {
+                _File = await _Folder.CreateFileAsync(_FileName, _Options);
+                Initialized = true;
+            }
+            catch (Exception ex)
+            {
+                Initialized = false;
+                NotifyLogError("Error clearing log file.\n Exception: " + ex.GetType().Name + "\n" + ex.Message);
+            }
+        }
+  
+        public async Task WriteLine(string line)
+        {
+            try
+            {
+                if(Initialized)
+                   await FileIO.WriteTextAsync(_File, line);
+            }
+            catch (Exception ex)
+            {
+                NotifyLogError("Error writing to log file.\n Exception: "+ ex.GetType().Name+"\n"+ex.Message);
+            }
+        }
+
+        public async Task<string> Read()
+        {
+            try
+            {
+                _File = await _Folder.GetFileAsync(_FileName);
+                return await FileIO.ReadTextAsync(_File);
+            }
+            catch(Exception ex)
+            {
+                NotifyLogError("Error reading log file.\n Exception: " + ex.GetType().Name + "\n" + ex.Message);
+                return string.Empty;
+            }
+        }
+    }
+
+    public class LogEventArgs: EventArgs
+    {
+        public LogEventArgs() { }
+        public LogEventArgs(string message)
+        {
+            _Message = message;
+        }
+        private string _Message = string.Empty;
+        public string Message { get { return _Message; } }
+    }
+}
