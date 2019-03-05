d9a2e30cc9b667a1019f44b6ae9d22b8d82aee46
Alexander Zyuzin <alx.zyuzin@gmail.com>
2019-03-05 16:32:00 +0200

    Cleanup ObsevableObjectCollection class

--- Balboa/Common/ObservableObjectCollection.cs
+++ Balboa/Common/ObservableObjectCollection.cs
@@ -16,9 +16,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Reflection;
-using Windows.UI.Core;
-
 
 namespace Balboa.Common
 {
@@ -30,91 +27,7 @@ namespace Balboa.Common
 
         public void NotifyCollectionChanged()
         {
-
             this.CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-
-
         }
-
     }
-
-//public class ObservableObjectCollection<T>: ObservableCollection<T>
-//        where T : INotifyPropertyChanged
-//    {
- 
-// //       public override event NotifyCollectionChangedEventHandler CollectionChanged;
-
- 
-//        //public void Sort(GenericComparer<T> comparer)
-//        //{
-//        //    if (comparer == null) throw new ArgumentNullException(nameof(comparer));
-
-//        //    bool unsorted = false;
-//        //    do
-//        //    {
-//        //        unsorted = false;
-//        //        int i = 0;
-//        //        int j = 1;
-
-//        //        while (j < Count)
-//        //        {
-//        //           if (comparer.Compare(this[i],this[j]) == 1)
-//        //           {
-//        //               Move(j, i);
-//        //               unsorted = true;
-//        //           }
-//        //           i++;
-//        //           j++;
-//        //       }
-//        //    }
-//        //    while (unsorted);
-//        //    CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-//        // }
-
-//        /// <summary>
-//        /// Возвращает отсортированный список уникальных значений заданного поля коллекции
-//        /// </summary>
-//        /// <returns></returns>
-//        //public List<string> GetFieldValues(string fieldName)
-//        //{
-//        //    List<string> values = new List<string>();
-//        //    Type type = typeof(T); 
-//        //    PropertyInfo propertyInfo = type.GetRuntimeProperty(fieldName);
-//        //    if (propertyInfo == null)
-//        //    {
-//        //        string message = string.Format(CultureInfo.InvariantCulture,"Not found property {0} in type {1}", fieldName, type.Name);
-//        //        throw new BalboaException("ObservableObjectCollection", "GetFieldValues", "135", message);
-//        //    }
-
-//        //    for (int i=0; i<Count; i++)
-//        //    {
-//        //        string value = (string)propertyInfo.GetValue(this[i]);
-//        //        if (!values.Contains(value))
-//        //            values.Add(value);              
-//        //    }
-//        //    values.Sort();
-
-//        //    return values;  
-//        //}
-
-//        //public bool Contains(string fieldName, object fieldValue)
-//        //{
-//        //    if (fieldName == null) throw new ArgumentNullException(nameof(fieldName));
-//        //    if (fieldValue == null) throw new ArgumentNullException(nameof(fieldValue));
- 
-//        //    Type type = typeof(T);
-//        //    PropertyInfo propertyInfo = type.GetRuntimeProperty(fieldName);
-
-//        //    if (propertyInfo == null) throw new ArgumentNullException(nameof(propertyInfo));
- 
-//        //    for (int i = 0; i < this.Count; i++)
-//        //    {
-//        //        object value = propertyInfo.GetValue(this[i]);
-//        //        if (value.ToString() == fieldValue.ToString())
-//        //            return true;
-//        //    }
-//        //    return false;
-//        //}
-       
-//    }
 }
